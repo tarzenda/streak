e89963503c2175a38a9513244d7eff540ab6c96b
tarzenda <code@tarzenda.net>
2014-10-05 01:16:24 -0500

    Enumerate IStreakAlgo implementations.

--- StreakShared/IStreakAlgo.cs
+++ StreakShared/IStreakAlgo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,22 @@ namespace Tarzenda.Streak
     public interface IStreakAlgo
     {
         StreakResults Calculate(int n, int k);
+    }
+
+    public static class IStreakAlgoExtensions
+    {
+        public static List<Type> ListAlgos()
+        {
+            Type iface = typeof(IStreakAlgo);
+            List<Type> types = new List<Type>();
+            foreach (Type t in Assembly.GetExecutingAssembly().GetTypes())
+            {
+                if (t.IsClass && !t.IsAbstract && iface.IsAssignableFrom(t))
+                {
+                    types.Add(t);
+                }
+            }
+            return types;
+        }
     }
 }
